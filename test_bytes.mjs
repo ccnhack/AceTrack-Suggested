5aa69f0b70d1fbf80a7431e8cf3afea5811a7918
Shashank Shekhar <shashankshekhar@Shashanks-MacBook-Pro.local>
2026-03-25 22:16:36 +0530

    Security: Purge hardcoded secrets and update gitignore

--- test_bytes.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-const lines = fs.readFileSync('screens/AdminHubScreen.js', 'utf8').split('\n');
-const line970 = lines[969];
-console.log("Line 970:");
-console.log(line970);
-console.log(Array.from(line970).map(c => c.charCodeAt(0)));
